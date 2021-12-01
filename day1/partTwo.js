a9388e6e20779d85ef2617b7d5c0aec297331b75
Guillaume BILLEY <guilbill@gmail.com>
2021-12-01 13:44:58 +0100

    Day1 part 2

--- day1/partTwo.js
+++ day1/partTwo.js
@@ -3,32 +3,16 @@ const fs = require('fs');
 const input = fs.readFileSync(`${__dirname}/inputPart1.txt`, 'utf8');
 const lines = input.split('\n').map(line => parseInt(line));
 
-// Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
-// If the input is
-// 199  A
-// 200  A B
-// 208  A B C
-// 210    B C D
-// 200      C D
-// 207        D
-// The sums are:
-// 199 + 200 + 208 = 607
-// 200 + 208 + 210 = 618
-// 208 + 210 + 200 = 618
-// 210 + 200 + 207 = 607
-
 const windows = lines.map((_, index) => {
     if (index + 3 >= lines.length) {
         return;
     }
     return lines[index] + lines[index + 1] + lines[index + 2];
 }).filter(window => window);
-console.log(windows);
+
 // Count the number of lines that are greater than the previous line.
 const nbOfValuesGreaterThanPrevious = windows.reduce((count, _, index) => {
-    const currentWindow = windows[index];
-    const previousWindow = windows[index - 1];
-    if (index > 0 && currentWindow > previousWindow) {
+    if (index > 0 && windows[index] >  windows[index - 1]) {
         count++;
     }
     return count;
